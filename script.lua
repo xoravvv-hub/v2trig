7ef0b8382fb53e903c0cf347771c8bc5dda28de2
xoravvv-hub <xo.ravvv@gmail.com>
2026-03-17 19:42:38 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -4,7 +4,7 @@ local HotkeyToggle = true
 local HoldClick = true
 
 -- Webhook
-local WebhookURL = "YOUR_WEBHOOK_URL_HERE"
+local WebhookURL = "https://discord.com/api/webhooks/1483425946961973308/fPVWe9pRrLskkkhnI-qF8RKHjCiSOCiKd__Kk6lwr5Vg4lvd8Fb0pQR9G26bttteIdBi"
 
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
